0590efc4850352f0c09cc64725fb8a1c1d4f6627
tonikelope <tonikelope@gmail.com>
2023-01-13 13:59:02 +0100

    0.39
    MoveToAnotherAcount dialog tunning

--- src/main/java/com/tonikelope/megadoctor/Main.java
+++ src/main/java/com/tonikelope/megadoctor/Main.java
@@ -35,7 +35,7 @@ import javax.swing.JTextArea;
  */
 public class Main extends javax.swing.JFrame {
 
-    public final static String VERSION = "0.37";
+    public final static String VERSION = "0.39";
     public final static ThreadPoolExecutor THREAD_POOL = (ThreadPoolExecutor) Executors.newCachedThreadPool();
     public final static String MEGA_CMD_URL = "https://mega.io/cmd";
     public final static String MEGA_CMD_WINDOWS_PATH = "C:\\Users\\" + System.getProperty("user.name") + "\\AppData\\Local\\MEGAcmd";
