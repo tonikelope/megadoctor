3a21bff87e969da630aaec9fc8b59a3a5fcf085c
tonikelope <tonikelope@gmail.com>
2023-02-01 16:38:53 +0100

    0.71
    Gui tunning

--- src/main/java/com/tonikelope/megadoctor/Helpers.java
+++ src/main/java/com/tonikelope/megadoctor/Helpers.java
@@ -636,7 +636,13 @@ public class Helpers {
                 public void actionPerformed(ActionEvent ae) {
                     if (txtArea.isEnabled() && txtArea.getSelectedText() != null && !txtArea.getSelectedText().isEmpty()) {
                         Helpers.threadRun(() -> {
-                            Main.MAIN_WINDOW.forceRefreshAccount(txtArea.getSelectedText().trim(), "Force refresh", true, true);
+
+                            String email = Helpers.extractFirstEmailFromtext(txtArea.getSelectedText());
+
+                            if (email != null) {
+
+                                Main.MAIN_WINDOW.forceRefreshAccount(email, "Force refresh", true, true);
+                            }
                         });
                     }
                 }
