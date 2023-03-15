228c0bb729a38c37daa68857433645c3594270f5
tonikelope <tonikelope@gmail.com>
2023-03-15 10:24:02 +0100

    1.75
    security upgrade fix

--- src/main/java/com/tonikelope/megadoctor/Helpers.java
+++ src/main/java/com/tonikelope/megadoctor/Helpers.java
@@ -316,8 +316,10 @@ public class Helpers {
         String[] whoami = Helpers.runProcess(new String[]{"mega-whoami"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null, true);
 
         if (whoami[1].contains("security needs upgrading")) {
+            Helpers.runProcess(new String[]{"mega-reload"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
             Helpers.runProcess(new String[]{"mega-confirm", "--security"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
-            whoami = Helpers.runProcess(new String[]{"mega-whoami"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
+            Main.MAIN_WINDOW.logout(false);
+            return megaWhoami();
         }
 
         if (Integer.parseInt(whoami[2]) == 0) {
--- src/main/java/com/tonikelope/megadoctor/Main.java
+++ src/main/java/com/tonikelope/megadoctor/Main.java
@@ -57,7 +57,7 @@ import javax.swing.UIManager;
  */
 public class Main extends javax.swing.JFrame {
 
-    public final static String VERSION = "1.74";
+    public final static String VERSION = "1.75";
     public final static int MESSAGE_DIALOG_FONT_SIZE = 20;
     public final static int MEGADOCTOR_ONE_INSTANCE_PORT = 32856;
     public final static ThreadPoolExecutor THREAD_POOL = (ThreadPoolExecutor) Executors.newCachedThreadPool();
@@ -368,8 +368,10 @@ public class Main extends javax.swing.JFrame {
             String[] login_session_output = Helpers.runProcess(new String[]{"mega-login", MEGA_SESSIONS.get(email)}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null, true);
 
             if (login_session_output[1].contains("security needs upgrading")) {
+                Helpers.runProcess(new String[]{"mega-reload"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
                 Helpers.runProcess(new String[]{"mega-confirm", "--security"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
-                login_session_output = Helpers.runProcess(new String[]{"mega-login", MEGA_SESSIONS.get(email)}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
+                logout(false);
+                return login(email);
             }
 
             if (Integer.parseInt(login_session_output[2]) != 0) {
@@ -381,8 +383,10 @@ public class Main extends javax.swing.JFrame {
                 String[] login = Helpers.runProcess(new String[]{"mega-login", email, Helpers.escapeMEGAPassword(password)}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null, true);
 
                 if (login[1].contains("security needs upgrading")) {
+                    Helpers.runProcess(new String[]{"mega-reload"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
                     Helpers.runProcess(new String[]{"mega-confirm", "--security"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
-                    login = Helpers.runProcess(new String[]{"mega-login", MEGA_SESSIONS.get(email)}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
+                    logout(false);
+                    return login(email);
                 }
 
                 if (Integer.parseInt(login[2]) != 0) {
@@ -403,8 +407,10 @@ public class Main extends javax.swing.JFrame {
             String[] login = Helpers.runProcess(new String[]{"mega-login", email, Helpers.escapeMEGAPassword(password)}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null, true);
 
             if (login[1].contains("security needs upgrading")) {
+                Helpers.runProcess(new String[]{"mega-reload"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
                 Helpers.runProcess(new String[]{"mega-confirm", "--security"}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
-                login = Helpers.runProcess(new String[]{"mega-login", MEGA_SESSIONS.get(email)}, Helpers.isWindows() ? MEGA_CMD_WINDOWS_PATH : null);
+                logout(false);
+                return login(email);
             }
 
             if (Integer.parseInt(login[2]) != 0) {
