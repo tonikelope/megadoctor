73fd1cb203fb0666016f0a6a99c44a24c16be235
tonikelope <tonikelope@gmail.com>
2023-03-19 21:07:39 +0100

    1.85
    msg fix

--- src/main/java/com/tonikelope/megadoctor/Main.java
+++ src/main/java/com/tonikelope/megadoctor/Main.java
@@ -57,7 +57,7 @@ import javax.swing.UIManager;
  */
 public class Main extends javax.swing.JFrame {
 
-    public final static String VERSION = "1.84";
+    public final static String VERSION = "1.85";
     public final static int MESSAGE_DIALOG_FONT_SIZE = 20;
     public final static int MEGADOCTOR_ONE_INSTANCE_PORT = 32856;
     public final static ThreadPoolExecutor THREAD_POOL = (ThreadPoolExecutor) Executors.newCachedThreadPool();
@@ -2299,7 +2299,7 @@ public class Main extends javax.swing.JFrame {
 
                                 bye();
                             }
-                        } else if (Helpers.mostrarMensajeInformativoSINO(this, "WARNING: RUNNING TRANSFERENCE PORGRESS WILL BE LOST") == 0) {
+                        } else if (Helpers.mostrarMensajeInformativoSINO(this, "WARNING: RUNNING TRANSFERENCE PROGRESS WILL BE LOST") == 0) {
                             bye();
                         }
                     } else {
