2dd3caf8537f76fd47c446a3d0e909cffe3519f8
tonikelope <tonikelope@gmail.com>
2023-09-13 13:35:56 +0200

    2.55
    FileSplitter fix

--- src/main/java/com/tonikelope/megadoctor/Main.java
+++ src/main/java/com/tonikelope/megadoctor/Main.java
@@ -62,7 +62,7 @@ import javax.swing.UIManager;
  */
 public class Main extends javax.swing.JFrame {
 
-    public final static String VERSION = "2.54";
+    public final static String VERSION = "2.55";
     public final static int MESSAGE_DIALOG_FONT_SIZE = 20;
     public final static int MEGADOCTOR_ONE_INSTANCE_PORT = 32856;
     public final static ThreadPoolExecutor THREAD_POOL = (ThreadPoolExecutor) Executors.newCachedThreadPool();
@@ -290,6 +290,8 @@ public class Main extends javax.swing.JFrame {
             while (!_closing) {
 
                 if (!FILE_SPLITTER_TASKS.isEmpty()) {
+
+                    saveFileSplitterTasks();
 
                     try {
                         Object[] task = (Object[]) FILE_SPLITTER_TASKS.peek();
